73b82f7de48ff1ecc4486523da070aff86cb201b
artemkolodko <temakolodko@gmail.com>
2022-11-21 15:32:20 +0000

    Add support .1 sub-domains

--- background.js
+++ background.js
@@ -1,10 +1,25 @@
 const MatchHost = '.1'
-const RedirectUrl = 'https://one.country'
+const RedirectProtocol = 'https://'
+const RedirectHost = 'one.country'
+
+function parseUrl (urlStr) {
+    if(urlStr.includes(`${MatchHost}&`)) {
+        const url = new URL(urlStr)
+        if(url) {
+            const queryParam = url.searchParams.get('q')
+            if(queryParam) {
+                const [subDomains] = queryParam.split(MatchHost)
+                return `${RedirectProtocol}${subDomains ? subDomains + '.' : ''}${RedirectHost}`
+            }
+        }
+    }
+}
 
 chrome.webRequest.onBeforeRequest.addListener((data) => {
-    if(data.url.includes(`${MatchHost}&`)) {
+    const redirectUrl = parseUrl(data.url)
+    if(redirectUrl) {
         return {
-            redirectUrl: RedirectUrl
+            redirectUrl
         }
     }
 }, {urls: ["<all_urls>"]}, ["blocking"])
