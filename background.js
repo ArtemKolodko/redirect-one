2d037f1f942853511f262b47cda042b491c5e6c6
artemkolodko <temakolodko@gmail.com>
2022-12-02 20:37:15 +0000

    Add support for google, bing and brave search

--- background.js
+++ background.js
@@ -4,11 +4,17 @@ const RedirectHost = '1.country'
 function parseUrl (urlStr) {
     const url = new URL(urlStr)
     if(url) {
-        const queryParam = url.searchParams.get('q') || ''
-        const isRedirectedFromUrl = url.searchParams.get('sourceid') === 'chrome'
+        const { hostname } = url
+        const queryParam = url.searchParams.get('q') || url.searchParams.get('p') || ''
+
+        const isGoogle = hostname.includes('google') && url.searchParams.get('sourceid') === 'chrome'
+        const isBrave = hostname.includes('brave') && url.searchParams.get('source') === 'desktop'
+        const isBing = hostname.includes('bing') && url.searchParams.get('FORM') === 'CHROMN'
+
+        const isRedirectedFromUrlSearch = isGoogle || isBrave || isBing
 
         // Do not handle requests coming from main google search page
-        if(isRedirectedFromUrl && queryParam.includes(TargetHost)) {
+        if(isRedirectedFromUrlSearch && queryParam.includes(TargetHost)) {
             const [prefix = '', postfix = ''] = queryParam.split(TargetHost)
             const domain = prefix.replace(/(http(s?)):\/\//, '')
             return `https://${domain ? domain + '.' : ''}${RedirectHost}${postfix}`
